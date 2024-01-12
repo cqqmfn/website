48e95b7fbc68c9ec6c351f7b87f563f3e6c6b999
chenjiaji <798274150@qq.com>
2024-01-12 11:37:15 +0800

    feat: upload file.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
-import './style.css'
+// import './style.css'
 import App from './App.vue'
+import router from './router'
+import 'virtual:windi.css'
+import '~/assets/styles/index.scss'
 
-createApp(App).mount('#app')
+const app = createApp(App)//根组件
+app.use(router)//路由
+app.mount('#app')//容器
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import WindiCSS from 'vite-plugin-windicss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  resolve: {//路径别名
+    alias: {
+      '~': resolve(__dirname,'src')
+    }
+  },
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: [//添加插件
+      'vue',
+      'vue/macros',
+      'vue-router'
+      ]
+    }),
+    Components({ /* options */ }),
+    WindiCSS(),
+  ],
 })
